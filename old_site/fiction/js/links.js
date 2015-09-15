46f0349078a5363dd788cdb608ebcbcf9f534187
Michaela Stone Cross <mcross1878@gmail.com>
2015-09-14 21:02:45 -0700

    backup

---
+++ old_site/fiction/js/links.js
@@ -0,0 +1,92 @@
+
+
+
+ $(document).ready(function () {
+       $('#story5').on('mouseenter', function () {
+           $('#kings').fadeIn("4000");
+           $('#title').hide();
+           $(this).css({
+               "text-decoration": "underline"
+           });
+       }).on('mouseleave', function () {
+           $('#kings').fadeOut("4000");
+           $('#title').fadeIn("6000");
+           $(this).css({
+               "text-decoration": ''
+           });
+       });;
+   });
+
+ $(document).ready(function () {
+       $('#story1').on('mouseenter', function () {
+           $('#hearteater').fadeIn("4000");
+           $('#title').hide();
+           $(this).css({
+               "text-decoration": "underline"
+           });
+       }).on('mouseleave', function () {
+           $('#hearteater').fadeOut("4000"
+            );
+           $('#title').fadeIn("6000");
+           $(this).css({
+               "text-decoration": ''
+           });
+       });;
+   });
+
+ $(document).ready(function () {
+       $('#story2').on('mouseenter', function () {
+           $('#elzvar').fadeIn("4000");
+           $('#title').hide(
+            );
+           $(this).css({
+               "text-decoration": "underline"
+           });
+       }).on('mouseleave', function () {
+           $('#elzvar').fadeOut("4000"
+            );
+           $('#title').fadeIn("6000"
+            );
+           $(this).css({
+               "text-decoration": ''
+           });
+       });;
+   });
+
+  $(document).ready(function () {
+       $('#story3').on('mouseenter', function () {
+           $('#summeraftersunset').fadeIn("4000"
+            );
+           $('#title').hide(
+            );
+           $(this).css({
+               "text-decoration": "underline"
+           });
+       }).on('mouseleave', function () {
+           $('#summeraftersunset').fadeOut("4000"
+            );
+           $('#title').fadeIn("6000"
+            );
+           $(this).css({
+               "text-decoration": ''
+           });
+       });;
+   });
+
+  $(document).ready(function () {
+       $('#story4').on('mouseenter', function () {
+           $('#draupadi').fadeIn("4000");
+           $('#title').hide();
+           $(this).css({
+               "text-decoration": "underline"
+           });
+       }).on('mouseleave', function () {
+           $('#draupadi').fadeOut("4000");
+           $('#title').fadeIn("6000");
+           $(this).css({
+               "text-decoration": ''
+           });
+       });;
+   });
+
+  console.log("tacos")
